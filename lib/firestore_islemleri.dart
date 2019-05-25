750491a7cd7852d7e88c8f2800900108067e155d
Emre Altunbilek <emrealtunbilek@gmail.com>
2019-05-25 23:32:01 +0300

    firestore update delete ve okuma işşlemleri anlatıldı

--- lib/firestore_islemleri.dart
+++ lib/firestore_islemleri.dart
@@ -22,7 +22,22 @@ class _FirestoreIslemleriState extends State<FirestoreIslemleri> {
               child: Text("Veri Ekle"),
               color: Colors.green,
               onPressed: _veriEkle,
-            )
+            ),
+            RaisedButton(
+              child: Text("Transaction Ekle"),
+              color: Colors.blue,
+              onPressed: _transactionEkle,
+            ),
+            RaisedButton(
+              child: Text("Veri Sil"),
+              color: Colors.red,
+              onPressed: _veriSil,
+            ),
+            RaisedButton(
+              child: Text("Veri Oku"),
+              color: Colors.pink,
+              onPressed: _veriOku,
+            ),
           ],
         ),
       ),
@@ -59,5 +74,94 @@ class _FirestoreIslemleriState extends State<FirestoreIslemleri> {
     _firestore
         .document("users/$yeniKullaniciID")
         .setData({'yas': 30, 'userID': '$yeniKullaniciID'});
+
+    _firestore.document("users/emre_altunbilek").updateData({
+      'okul': 'ege üniversitesi',
+      'yas': 60,
+      'eklenme': FieldValue.serverTimestamp(),
+      'begeniSayisi': FieldValue.increment(10)
+    }).then((v) {
+      debugPrint("emre güncellendi");
+    });
+  }
+
+  void _transactionEkle() {
+    final DocumentReference emreRef =
+        _firestore.document("users/emre_altunbilek");
+
+    debugPrint("doc id:" + emreRef.documentID);
+
+    _firestore.runTransaction((Transaction transaction) async {
+      DocumentSnapshot emreData = await emreRef.get();
+
+      debugPrint("doc id:" + emreData.documentID);
+
+      if (emreData.exists) {
+        var emreninParasi = emreData.data['para'];
+
+        if (emreninParasi > 100) {
+          await transaction.update(emreRef, {'para': (emreninParasi - 100)});
+          await transaction.update(_firestore.document("users/hasan_yilmaz"),
+              {'para': FieldValue.increment(100)});
+        } else {
+          debugPrint("yetersiz bakiye");
+        }
+      } else {
+        debugPrint("emre dökümanı yok");
+      }
+    });
+  }
+
+  void _veriSil() {
+    //Döküman silme
+    _firestore.document("users/ayse").delete().then((aa) {
+      debugPrint("ayse silindi");
+    }).catchError((e) => debugPrint("Silerken hata cıktı" + e.toString()));
+
+    _firestore
+        .document("users/hasan_yilmaz")
+        .updateData({'cinsiyet': FieldValue.delete()}).then((aa) {
+      debugPrint("cinsiyet silindi");
+    }).catchError((e) => debugPrint("Silerken hata cıktı" + e.toString()));
+  }
+
+  Future _veriOku() async {
+    //tek bir dökümanın okunması
+    DocumentSnapshot documentSnapshot =
+        await _firestore.document("users/emre_altunbilek").get();
+    debugPrint("Döküman id:" + documentSnapshot.documentID);
+    debugPrint("Döküman var mı:" + documentSnapshot.exists.toString());
+    debugPrint("Döküman string: " + documentSnapshot.toString());
+    debugPrint("bekleyen yazma var mı:" +
+        documentSnapshot.metadata.hasPendingWrites.toString());
+    debugPrint("cacheden mi geldi:" +
+        documentSnapshot.metadata.isFromCache.toString());
+    debugPrint("cacheden mi geldi:" + documentSnapshot.data.toString());
+    debugPrint("cacheden mi geldi:" + documentSnapshot.data['ad']);
+    debugPrint("cacheden mi geldi:" + documentSnapshot.data['para'].toString());
+    documentSnapshot.data.forEach((key, deger) {
+      debugPrint("key : $key deger :deger");
+    });
+
+    //koleksiyonun okunması
+    _firestore.collection("users").getDocuments().then((querySnapshots) {
+      debugPrint("User koleksiyonundaki eleman sayısı:" +
+          querySnapshots.documents.length.toString());
+
+      for (int i = 0; i < querySnapshots.documents.length; i++) {
+        debugPrint(querySnapshots.documents[i].data.toString());
+      }
+
+      //anlık değişikliklerin dinlenmesi
+      DocumentReference ref =
+          _firestore.collection("users").document("emre_altunbilek");
+      ref.snapshots().listen((degisenVeri) {
+        debugPrint("anlık :" + degisenVeri.data.toString());
+      });
+
+      _firestore.collection("users").snapshots().listen((snap) {
+        debugPrint(snap.documents.length.toString());
+      });
+    });
   }
 }
